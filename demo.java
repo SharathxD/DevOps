b0abdf604a68a50e4fbb01ca6014d60f19581980
Sharath Gowda <msharathgowda7@gmail.com>
2025-02-13 15:59:31 +0530

    after running on ubuntu

--- demo.java
+++ demo.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Demo code demoodeaskjdaskdaskjd");
+        HashSet<Integer> hm= new HashSet<>();
+        int arr[]={1,2,3,4,5,6,6};
+        for(int num:arr){
+            System.out.println(num);
+            System.out.println(hm.add(num));
+        }
+        System.out.println(hm);
+
     }
 }
